451adb00ee4a7d8de81e6a1366c461adeb7714a7
Paulo Henrique Costa <89080540+PauloHenriqCosta@users.noreply.github.com>
2025-04-15 19:46:34 +0000

    Adiciona página inicial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <>
+            <h1>Na pele</h1>
+            <a href="https://www.youtube.com/watch?v=saHcmtU9I-0" target="_blank" rel="noopener noreferrer">
+                <button>Assista no YouTube</button>
+            </a>
+        </>
+    );
+}
+
+export default Home;
