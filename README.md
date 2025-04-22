f25c559f18b2bd06fd95030d7e82904984616962
Paulo Henrique Costa <89080540+PauloHenriqCosta@users.noreply.github.com>
2025-04-22 19:23:47 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Meu primeiro projeto de ponta a ponta (curso.dev).
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 function Home() {
-    return (
-        <>
-            <h1>Na pele</h1>
-            <a href="https://www.youtube.com/watch?v=saHcmtU9I-0" target="_blank" rel="noopener noreferrer">
-                <button>Assista no YouTube</button>
-            </a>
-        </>
-    );
+  return (
+    <>
+      <h1>Na pele</h1>
+      <a
+        href="https://www.youtube.com/watch?v=saHcmtU9I-0"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button>Assista no YouTube</button>
+      </a>
+    </>
+  );
 }
 
 export default Home;
