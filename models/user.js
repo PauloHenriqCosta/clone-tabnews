3b08535b5cc336b36debbcbdeeec72e40c4bd5be
Paulo Henrique Costa <89080540+PauloHenriqCosta@users.noreply.github.com>
2025-07-04 13:35:13 +0000

    feat: create `user.findOneByUsername` method and `/api/v1/users/[username]`

--- models/user.js
+++ models/user.js
@@ -1,5 +1,37 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/errors.js";
+import { ValidationError, NotFoundError } from "infra/errors.js";
+
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const results = await database.query({
+      text: `
+        SELECT
+          *
+        FROM
+          users        
+        WHERE
+          LOWER(username)= LOWER($1)        
+        LIMIT 
+          1
+        ;`,
+      values: [username],
+    });
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        name: "NotFoundError",
+        message: "O username informado não foi encontrado no sistema.",
+        action: "Verifique se o username está digitado corretamente.",
+        status_code: 404,
+      });
+    }
+
+    return results.rows[0]; //retorna um objeto que retorna pro método e que entra pro controller e retorna na response.
+  }
+}
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
@@ -7,69 +39,70 @@ async function create(userInputValues) {
 
   const newUser = await runInsertQuery(userInputValues);
   return newUser;
+}
 
-  async function validateUniqueEmail(email) {
-    const results = await database.query({
-      text: `
-          SELECT
-            email
-          FROM
-            users
-          WHERE
-            LOWER(email)= LOWER($1)
-          `,
-      values: [email],
+async function validateUniqueEmail(email) {
+  const results = await database.query({
+    text: `
+        SELECT
+          email
+        FROM
+          users
+        WHERE
+          LOWER(email)= LOWER($1)
+        `,
+    values: [email],
+  });
+  if (results.rowCount > 0) {
+    throw new ValidationError({
+      message: "O email informado já está sendo usado",
+      action: "Utilize outro email para realizar o cadastro.",
     });
-    if (results.rowCount > 0) {
-      throw new ValidationError({
-        message: "O email informado já está sendo usado",
-        action: "Utilize outro email para realizar o cadastro.",
-      });
-    }
   }
+}
 
-  async function validateUniqueUserName(username) {
-    const results = await database.query({
-      text: `
-          SELECT
-            username
-          FROM
-            users
-          WHERE
-            LOWER(username) = LOWER($1)          
-          `,
-      values: [username],
+async function validateUniqueUserName(username) {
+  const results = await database.query({
+    text: `
+        SELECT
+          username
+        FROM
+          users
+        WHERE
+          LOWER(username) = LOWER($1)          
+        `,
+    values: [username],
+  });
+  if (results.rowCount > 0) {
+    throw new ValidationError({
+      message: "O nome de usuário informado já está sendo usado.",
+      action: "Utilize outro nome de usuário para realizar o cadastro.",
     });
-    if (results.rowCount > 0) {
-      throw new ValidationError({
-        message: "O nome de usuário informado já está sendo usado.",
-        action: "Utilize outro nome de usuário para realizar o cadastro.",
-      });
-    }
   }
+}
 
-  async function runInsertQuery(userInputValues) {
-    const results = await database.query({
-      text: `
-        INSERT INTO 
-          users (username, email, password) 
-        VALUES 
-          ($1, $2, $3)
-        RETURNING
-          *
-          ;`,
-      values: [
-        userInputValues.username,
-        userInputValues.email,
-        userInputValues.password,
-      ],
-    });
-    return results.rows[0];
-  }
+async function runInsertQuery(userInputValues) {
+  const results = await database.query({
+    text: `
+      INSERT INTO 
+        users (username, email, password) 
+      VALUES 
+        ($1, $2, $3)
+      RETURNING
+        *
+        ;`,
+    values: [
+      userInputValues.username,
+      userInputValues.email,
+      userInputValues.password,
+    ],
+  });
+  return results.rows[0];
 }
 
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
